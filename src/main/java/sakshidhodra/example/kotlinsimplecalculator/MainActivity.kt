69d7e69b37f8009750efa84ea94f3d25f6664a57
sakshid1998 <51333260+sakshid1998@users.noreply.github.com>
2020-07-26 20:32:14 +0530

    Add files via upload

---
+++ src/main/java/sakshidhodra/example/kotlinsimplecalculator/MainActivity.kt
@@ -0,0 +1,41 @@
+package sakshidhodra.example.kotlinsimplecalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun sum(view: View)
+    {
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
+        val result = a+b
+        textView.text="Result: $result"
+    }
+    fun sub(view: View)
+    {
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
+        val result = a-b
+        textView.text="Result: $result"
+    }
+    fun multiply(view: View)
+    {
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
+        val result = a*b
+        textView.text="Result: $result"
+    }
+    fun div(view: View)
+    {
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
+        val result = a/b
+        textView.text="Result: $result"
+    }
+}
